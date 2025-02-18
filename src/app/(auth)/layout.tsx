94a3ec5493078bdc95fc21b6132fea9bac1c69ec
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-02-19 00:55:02 +0200

    layout updated 1

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex justify-between items-center min-h-screen p-4">
+    <div className="flex justify-center gap-16 items-center min-h-screen p-4">
       <div className="hidden lg:block w-[52%] bg-muted shadow-lg py-12 pl-12 rounded-xl">
         <div className="bg-background ml-auto h-[570px] lg:w-[500px] xl:w-[530px] py-2 pl-2 rounded-lg">
           <div className="auth-bg" />
