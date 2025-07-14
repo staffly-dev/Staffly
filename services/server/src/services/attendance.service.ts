cd700dad6836e4bc8c3be2c0d422163f74a4b936
AndroRefaat <androrefaat44@gmail.com>
2025-07-14 18:06:00 +0300

    add Api gateway

---
+++ services/server/src/services/attendance.service.ts
@@ -0,0 +1,95 @@
+
+import Attendance, { IAttendance } from "../models/attendance.model";
+import EmployeeModel from "../models/employee.model";
+import { NotFoundException } from "../utils/app-error";
+
+export const recordCheckInService = async (
+  employeeId: string,
+  checkInTime?: Date 
+): Promise<IAttendance> => {
+
+  const finalCheckInTime = checkInTime || new Date();
+
+const employee = await EmployeeModel.findById(employeeId);
+if (!employee) throw new NotFoundException("Employee not found");
+
+  const attendance = await Attendance.findOne({
+    employeeId,
+    date: {
+      $gte: new Date(new Date().setHours(0, 0, 0, 0)),
+      $lt: new Date(new Date().setHours(23, 59, 59, 999)),
+    },
+  });
+  if (attendance) throw new NotFoundException("Check-in already recorded for today");
+
+  const newAttendance = new Attendance({
+    employeeId,
+    checkInTime: finalCheckInTime,
+    status: finalCheckInTime.getHours() < 9 ? "On Time" : "Late",   // time : 9AM
+  });
+  return await newAttendance.save();
+};
+
+export const getAllAttendanceService = async (): Promise<IAttendance[]> => {
+
+  return await Attendance.find().populate({
+    path: "employeeId",
+    model: EmployeeModel,
+    select: "firstName lastName designation employeeType"
+  });
+
+};
+
+
+export const getAttendanceService = async (id: string): Promise<IAttendance> => {
+  const attendance = await Attendance.findById(id).populate({
+    path: "employeeId",
+    model: EmployeeModel,
+    select: "firstName lastName designation employeeType"
+  });
+  if (!attendance) throw new NotFoundException("Attendance not found");
+  return attendance;
+};
+
+
+export const searchAttendanceService = async (firstName?: string, lastName?: string): Promise<IAttendance[]> => {
+  const matchStage: any = {};
+
+  if (firstName) {
+    matchStage['employee.firstName'] = { $regex: firstName, $options: 'i' };
+  }
+
+  if (lastName) {
+    matchStage['employee.lastName'] = { $regex: lastName, $options: 'i' };
+  }
+
+  const result = await Attendance.aggregate([
+    {
+      $lookup: {
+        from: "employees",
+        localField: "employeeId",
+        foreignField: "_id",
+        as: "employee"
+      }
+    },
+    { $unwind: "$employee" },
+    { $match: matchStage },
+    {
+      $project: {
+        _id: 1,
+        checkInTime: 1,
+        status: 1,
+        date: 1,
+        employeeId: {
+          _id: "$employee._id",
+          firstName: "$employee.firstName",
+          lastName: "$employee.lastName",
+          designation: "$employee.designation",
+          employeeType: "$employee.employeeType"
+        }
+      }
+    }
+  ]);
+
+  return result;
+};
