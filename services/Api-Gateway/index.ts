7fc87634f3078bd3d0b16a04ef007c8c74f3e0b5
AndroRefaat <androrefaat44@gmail.com>
2025-07-28 04:46:46 +0300

    edit on API Gateway structure

--- services/Api-Gateway/index.ts
+++ services/Api-Gateway/index.ts
@@ -1,16 +1,12 @@
 import "dotenv/config";
 import express, { Request, Response, NextFunction } from 'express';
-import { asyncHandler } from "server/middlewares/api/asyncHandler.middleware";
-import { HTTPSTATUS } from "server/config/http.config";
-import { errorHandler } from "server/middlewares/errors/errorHandler.middleware";
-import { Env } from "server/config/env.config";
-import connectDatabase from "server/config/database.config";
-import { swaggerUi, swaggerSpec } from "server/swagger";
-
-import { swaggerAuth } from "server/middlewares/docs/swagger-docs.middleware";
-
-import { applySecurityStack, securityStack } from "server/middlewares/security";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { Env } from "./config/env.config";
+import { asyncHandler } from "./middlewares/api/asyncHandler.middleware";
+import { errorHandler } from "./errors/errorHandler.middleware";
+import { applySecurityStack, securityStack } from "./middlewares/security";
+
 
 const app = express();
 
@@ -35,11 +31,7 @@ app.get(
   })
 );
 
-if (Env.NODE_ENV !== 'development') {
-  app.use(`/api-docs`, swaggerAuth, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-} else {
-  app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
+
 
 // localhost http://localhost:4005/server/auth/login ==> direct to proxy http://localhost:4004/auth/login
 app.use('/server', createProxyMiddleware({
@@ -65,5 +57,4 @@ app.use(errorHandler);
 app.listen(Env.API_GATEWAY_PORT, async () => {
   console.log(`Server listening on port ${Env.API_GATEWAY_PORT} in development`);
   console.log(`🔒 Security stack enabled with ${securityStack.length} protection layers`);
-  await connectDatabase();
 });
---
+++ services/Api-Gateway/utils/app-error.ts
@@ -0,0 +1,40 @@
+import { HTTPSTATUS, HttpStatusCodeType } from "../config/http.config";
+
+export class AppError extends Error {
+  public statusCode: HttpStatusCodeType;
+  constructor(message: string, statusCode = HTTPSTATUS.INTERNAL_SERVER_ERROR) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class InternalServerException extends AppError {
+  constructor(message = "Internal Server Error") {
+    super(message, HTTPSTATUS.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export class NotFoundException extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, HTTPSTATUS.NOT_FOUND);
+  }
+}
+
+export class BadRequestException extends AppError {
+  constructor(message = "Bad Request") {
+    super(message, HTTPSTATUS.BAD_REQUEST);
+  }
+}
+
+export class ForbiddenException extends AppError {
+  constructor(message = "Forbidden") {
+    super(message, HTTPSTATUS.FORBIDDEN);
+  }
+}
+
+export class UnauthorizedException extends AppError {
+  constructor(message = "Unauthorized Access") {
+    super(message, HTTPSTATUS.UNAUTHORIZED);
+  }
+}
